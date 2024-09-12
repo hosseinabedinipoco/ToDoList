a4fe8755441261e8fd7fe80edf8a41daea3f6c7b
Hossein <hosseinabedinipoco@gmail.com>
2024-09-12 16:10:37 +0330

    register api

--- ToDoList/users/views.py
+++ ToDoList/users/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
+from .serializer import RegisterSerializer
 # Create your views here.
 class login(APIView):
     permission_class = [AllowAny]
@@ -21,6 +22,17 @@ class login(APIView):
         else:
             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         
-        
+
 class register(APIView):
-    pass
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = RegisterSerializer(request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'user': RegisterSerializer(user).data,
+                'message': 'User created successfully.'
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
